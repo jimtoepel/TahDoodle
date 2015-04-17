b8cca66ee08e43238667ac79ef718d8fa11a5aaa
jimtoepel <jim.toepel@gmail.com>
2015-04-16 19:42:57 -0700

    initial commit

---
+++ TahDoodle/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  TahDoodle
+//
+//  Created by Jim Toepel on 4/16/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
